2cb59c2a78cbb9279443726fe70e88b3a687d73d
Eric Wheeler <perl-api-octopart@z.ewheeler.org>
2022-08-05 17:43:24 -0700

    Added copyright, versions, warnings

--- lib/API/Octopart.pm
+++ lib/API/Octopart.pm
@@ -1,7 +1,32 @@
 #!/usr/bin/perl
 
+
+# This module is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+# 
+# This module is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# this module. If not, see <http://www.gnu.org/licenses/>.
+#
+#  Copyright (C) 2022- eWheeler, Inc. L<https://www.linuxglobal.com/>
+#  Originally written by Eric Wheeler, KJ7LNW
+#  All rights reserved.
+#
+#  All tradmarks, product names, logos, and brands are property of their
+#  respective owners and no grant or license is provided thereof.
+#
+
 package API::Octopart;
+$VERSION = 1.001;
+
+use 5.006;
 use strict;
+use warnings;
 
 use JSON;
 use LWP::UserAgent;
